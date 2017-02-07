264d8fac0525f4128ecd346990525ca489937e71
sanjeev <sanjunitr51@gmail.com>
2017-02-07 10:41:14 +0900

    Search in a binary tree by recursion

---
+++ madeEasy/chap6/3prob.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct tree{
+  int data;
+  struct tree *left, *right;
+} node;
+
+node *create() {
+  node* p;
+  int n;
+  printf("\nEnter data (Enter -1 if no child node) : ");
+  scanf("%d", &n);
+  if(n == -1) {
+    return NULL;
+  }
+  p = (node*)malloc(sizeof(node));
+  p->data = n;
+  printf("\nEnter left child of %d", n);
+  p->left = create();
+  printf("\nEnter right child of %d", n);
+  p->right = create();
+  return p;
+}
+
+bool find(node *p, int n) {
+  bool res = false;
+  if(p) {
+    if(p->data == n)
+      return true;
+    find(p->left, n);
+    find(p->right, n);
+  }
+  return res;
+}
+
+void main() {
+  node *root;
+  root = create();
+  int n;
+  printf("\nEnter the data you want to search ");
+  scanf("%d", &n);
+  bool res = find(root, n);
+  if(res)
+    printf("\n%d exists in tree.", n);
+  else
+    printf("\n%d does not exist in tree.", n);
+}
