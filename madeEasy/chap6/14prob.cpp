dbb99da185a7b4cd7562c651ba2a847690dde27e
sanjeev <sanjunitr51@gmail.com>
2017-02-09 16:39:52 +0900

    Count number of leaves in a tree

---
+++ madeEasy/chap6/14prob.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <queue>
+#include <stdlib.h>
+
+using namespace std;
+
+typedef struct tree {
+  int data;
+  struct tree *left, *right;
+} node;
+
+queue <node*> q;
+
+node *create() {
+  node* p;
+  int n;
+  printf("\nEnter data (Enter -1 if no child node) : ");
+  scanf("%d", &n);
+  if(n == -1) {
+    return NULL;
+  }
+  p = (node*)malloc(sizeof(node));
+  p->data = n;
+  printf("\nEnter left child of %d", n);
+  p->left = create();
+  printf("\nEnter right child of %d", n);
+  p->right = create();
+  return p;
+}
+
+int totalLeaves(node *p) {
+  if(!p)return 0;
+  q.push(p);
+  node *temp;
+  int count = 0;
+  while(!q.empty()) {
+    temp = q.front();
+    q.pop();
+    if (!temp->left && !temp->right) {
+        count++;
+    } else {
+      if(temp->left)
+        q.push(temp->left);
+      if(temp->right)
+        q.push(temp->right);
+    }
+  }
+  return count;
+}
+
+int main() {
+  node* root;
+  root = create();
+  printf("\nTree has %d leaves.", totalLeaves(root));
+  return 0;
+}
